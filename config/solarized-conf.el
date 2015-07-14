eec6630f1c3f5491fb75bbc08fb82f8c89c0a803
Varoun P <contact@varoun.com>
2015-07-14 11:36:58 +0530

    Initial attempt at getting the solarized theme to work with emacs live.

---
+++ config/solarized-conf.el
@@ -0,0 +1,4 @@
+;; Solarized theme configuration file
+(add-to-list 'load-path "~/.live-packs/solarized-pack/lib/app/")
+(add-to-list 'custom-theme-load-path "~/.live-packs/solarized-pack/lib/themes/")
+;(load-theme 'solarized-dark t)
